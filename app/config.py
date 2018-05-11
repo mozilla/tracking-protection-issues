01f7b8caad7fa9d60a49bee813c70e8077ca1d47
Mike Taylor <miket@mozilla.com>
2018-05-11 17:58:21 -0500

    Issue #16. Add some useless default values for env variables.
    This makes mocking requests less weird looking.

--- app/config.py
+++ app/config.py
@@ -6,12 +6,13 @@
 import os
 
 # The following environment variables are assumed to exist:
-OAUTH_TOKEN = os.environ.get('OAUTH_TOKEN', '')
+OAUTH_TOKEN = os.environ.get('OAUTH_TOKEN', 'test')
 PRODUCTION = os.environ.get('PRODUCTION', 0)
-REGION = os.environ.get('REGION', '')
-S3_BUCKET = os.environ.get("S3_BUCKET_NAME", '')
-S3_KEY = os.environ.get("S3_ACCESS_KEY", '')
-S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY", '')
+REGION = os.environ.get('REGION', 'test')
+REPO = os.environ.get('REPO', 'test/test')
+S3_BUCKET = os.environ.get("S3_BUCKET_NAME", 'test')
+S3_KEY = os.environ.get("S3_ACCESS_KEY", 'test')
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY", 'test')
 
 S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 SECRET_KEY = os.urandom(32)
